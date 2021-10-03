27895b0a7693f55aca00fc11768f8ad1f2da2f84
Baocheng Wang <baocheng0121@gmail.com>
2021-10-03 19:46:13 +0800

    finish model

--- learning_forum/models.py
+++ learning_forum/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Posts(models.Model):
+    user = models.ForeignKey('learning_profile.User', on_delete=models.CASCADE)
+    likes = models.IntegerField()
+    status = models.IntegerField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    content = models.CharField(max_length=255)
+    post_type = models.IntegerField()
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        db_table = 'posts'
+
+
+class Comments(models.Model):
+    post = models.ForeignKey('learning_profile.Posts', on_delete=models.CASCADE)
+    user = models.ForeignKey('learning_profile.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    content = models.CharField(max_length=300, blank=True, null=True)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        db_table = 'comments'
--- learning_goal/models.py
+++ learning_goal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Goals(models.Model):
+    post = models.ForeignKey('learning_forum.Posts', on_delete=models.CASCADE)
+    likes = models.IntegerField()
+    publish_status = models.IntegerField()
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        db_table = 'goals'
