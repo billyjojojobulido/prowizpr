633e13fd9c0bb537a2ac9db47d61ce69aea271fc
Baocheng Wang <baocheng0121@gmail.com>
2021-10-04 18:07:27 +0800

    add superuser and init the frontend framework

--- goal_project/urls.py
+++ goal_project/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path, re_path
 from django.contrib import admin
-from django.urls import path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('forum/', include('learning_forum.urls', namespace='forum')),
+    path('goal/', include('learning_goal.urls', namespace='goal')),
+    path('profile/', include('learning_profile.urls', namespace='profile')),
 ]
