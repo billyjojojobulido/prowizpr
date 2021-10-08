c28327d6e01763e9bdcb0ee2bc343d8b3558e3e4
jkan0240 <jkan0240@uni.sydney.edu.au>
2021-10-08 12:14:58 +0800

    form

--- learning_profile/urls.py
+++ learning_profile/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from django.contrib.auth.views import LoginView
 
 from . import views
-
 app_name = 'profile'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register')
 ]
--- learning_profile/views.py
+++ learning_profile/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, get_user_model
+from .forms import CustomUserCreationForm
 
-
+User = get_user_model()
 # Create your views here.
 
+
+def register(request):
+    if request.method != 'POST':
+        form = CustomUserCreationForm()
+    else:
+        form = CustomUserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect('profile:login')
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
