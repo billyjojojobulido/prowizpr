9c51d1d3a6362ff96541858ed3efbd42240ba4c8
Baocheng Wang <baocheng0121@gmail.com>
2021-10-13 17:22:35 +0800

    [newbrach] forum

--- learning_forum/urls.py
+++ learning_forum/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'forum'
 
 urlpatterns = [
-    path('show', views.show, name='show'),
+    path('comments', views.get_comments, name='comments'),
+    path('todo', views.get_todo, name='todo'),
+    path('like', views.like, name='like'),
+    path('comment', views.comment, name='comment'),
+    path('report', views.report, name='report'),
 ]
