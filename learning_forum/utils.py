2a8a1dc7a8c63f9d5e8c18ee6fa04c91922baac1
Baocheng Wang <baocheng0121@gmail.com>
2021-10-14 18:09:48 +0800

    utilis & const

---
+++ learning_forum/utils.py
@@ -0,0 +1,29 @@
+import time
+import const
+
+
+# time_format() format the time value to
+def time_format(t):
+    return time.strftime(const.TIME_FORMAT, t)
+
+
+# get_color() return the color with reference to the percentage
+def get_color(percentage):
+    # ensure percentage is in the range 0 - 1
+    if percentage < 0:
+        percentage = 0
+    elif percentage > 100:
+        percentage = 100
+
+    # assigning color
+    if percentage < 20:
+        return const.COLOR_PINK
+    elif percentage < 40:
+        return const.COLOR_BROWN
+    elif percentage < 60:
+        return const.COLOR_GREEN
+    elif percentage < 60:
+        return const.COLOR_BLUE
+    elif percentage <= 100:
+        return const.COLOR_PURPLE
+
