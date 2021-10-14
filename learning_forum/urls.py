ae9f4591d998c2e11b7a4663ab570195fedfa2e0
Baocheng Wang <bwan3675@uni.sydney.edu.au>
2021-10-14 17:03:27 +0800

    upload data from db

--- learning_forum/urls.py
+++ learning_forum/urls.py
@@ -6,7 +6,7 @@ app_name = 'forum'
 
 urlpatterns = [
     path('progress', views.get_progress, name='progress'),
-    path('comments', views.get_comments, name='comments'),
+    path('posts', views.get_posts, name='comments'),
     path('todo', views.get_todo, name='todo'),
     path('like', views.like, name='like'),
     path('comment', views.comment, name='comment'),
