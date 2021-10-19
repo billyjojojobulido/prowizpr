13e95f5a0333c917564a7a031ee5ad05a83c167f
jkan0240 <jkan0240@uni.sydney.edu.au>
2021-10-19 19:42:23 +0800

    modify the user model and develop register api

--- backend/learning_profile/models.py
+++ backend/learning_profile/models.py
@@ -5,7 +5,6 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     """redefine a new default user model"""
-    unikey = models.CharField(max_length=8, blank=True, null=True)
     account_status = models.IntegerField(blank=True, null=True)
     profile_image = models.CharField(max_length=255, blank=True, null=True)
     description = models.CharField(max_length=255, blank=True, null=True)
--- backend/learning_profile/views.py
+++ backend/learning_profile/views.py
@@ -1,19 +1,27 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, get_user_model
+from django.views.decorators.http import require_http_methods
+import json
+
 from .forms import CustomUserCreationForm
 
 User = get_user_model()
 # Create your views here.
 
 
+@require_http_methods(["POST"])
 def register(request):
-    if request.method != 'POST':
-        form = CustomUserCreationForm()
+    response = {}
+    form = CustomUserCreationForm(data=request.POST)
+
+    if form.is_valid():
+        new_user = form.save()
+        response["status"] = "success"
+        response["msg"] = "The user has successfully registered"
     else:
-        form = CustomUserCreationForm(data=request.POST)
-        if form.is_valid():
-            new_user = form.save()
-            login(request, new_user)
-            return redirect('profile:login')
-    context = {'form': form}
-    return render(request, 'registration/register.html', context)
+        error_info = form.errors.as_json()
+        data = json.loads(error_info)
+        response["status"] = "failed"
+        response["msg"] = tuple(data.items())[0][1]
+    return JsonResponse(response)
