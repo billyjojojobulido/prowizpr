453ca275f7acaff876369ca82f9f9bf4204a0b31
Baocheng Wang <bwan3675@uni.sydney.edu.au>
2021-10-21 12:00:31 +0800

    ajax refresh comments after made comment

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-<!--    <Forum />-->
-    <Goal />
+    <Forum />
+<!--    <Goal />-->
   </div>
 </template>
 
 <script>
-// import Forum from "@/components/forum/Forum";
-import Goal from "@/components/goal/Goal";
+import Forum from "@/components/forum/Forum";
+// import Goal from "@/components/goal/Goal";
 
 export default {
   name: 'App',
   components: {
-    // Forum
-    Goal
+    Forum
+    // Goal
   }
 }
 </script>
