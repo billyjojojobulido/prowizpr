a96ea98fc58ca5d37dc91f2270843f28d1dbd5dd
Baocheng Wang <baocheng0121@gmail.com>
2021-10-24 10:49:00 +0800

    update

--- backend/learning_goal/urls.py
+++ backend/learning_goal/urls.py
@@ -7,7 +7,7 @@ app_name = 'goal'
 urlpatterns = [
     path('show', views.show, name='show'),
     path('retrieve_task', views.retrieve_task, name='retrieve_task'),
-    path('add_goal', views.Add_Goal, name='add_goal'),
-    path('add_task', views.Add_Task, name='add_task'),
+    path('add_goal', views.add_goal, name='add_goal'),
+    path('add_task', views.add_task, name='add_task'),
 
 ]
