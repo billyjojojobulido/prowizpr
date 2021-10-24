411b31a456514f613c0ee7fa1ae43b861f6867ed
Baocheng Wang <baocheng0121@gmail.com>
2021-10-24 23:12:47 +0800

    email function

--- backend/learning_profile/urls.py
+++ backend/learning_profile/urls.py
@@ -6,5 +6,5 @@ app_name = 'profile'
 urlpatterns = [
     # path('', include('django.contrib.auth.urls')),
     path('login', views.login, name='login'),
-    path('register/', views.register, name='register')
+    path('password', views.find_password, name='password')
 ]
