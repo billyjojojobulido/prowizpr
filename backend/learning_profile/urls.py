1f36587a6cc0e8daf4ceaf163af5a6ff1cff297d
jkan0240 <jkan0240@uni.sydney.edu.au>
2021-10-29 22:54:47 +0800

    add csrf and logout

--- backend/learning_profile/urls.py
+++ backend/learning_profile/urls.py
@@ -6,11 +6,12 @@ app_name = 'profile'
 urlpatterns = [
     # path('', include('django.contrib.auth.urls')),
     path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
+    path('logout', views.user_exit, name='logout'),
     path('password', views.find_password, name='password'),
     path('resetpwd', views.verify_password, name='resetpwd'),
     path('changepwd', views.change_password, name='changepwd'),
     path('modify_profile', views.modify_basic_information, name='modify_profile'),
     path('information', views.view_profile, name='information'),
     path('upload_image', views.upload_image, name='upload_image'),
+    path('flush', views.test, name='flush')
 ]
