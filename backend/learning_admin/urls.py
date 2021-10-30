9e6208848f912d8fbbb42b6e5dad90e76266bdca
Baocheng Wang <bwan3675@uni.sydney.edu.au>
2021-10-30 16:53:50 +0800

    ban & uban user

--- backend/learning_admin/urls.py
+++ backend/learning_admin/urls.py
@@ -7,4 +7,5 @@ app_name = 'admins'
 urlpatterns = [
     path('get_users', views.get_users, name='get_users'),
     path('get_posts', views.get_posts, name='get_posts'),
+    path('ban_user', views.ban_user, name='ban_user'),
 ]
