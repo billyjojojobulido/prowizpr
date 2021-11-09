1527c960b213cf2fc3286d613bcceef82fa38bd8
Baocheng Wang <bwan3675@uni.sydney.edu.au>
2021-11-09 13:15:35 +0800

    test cases in Forum

--- backend/learning_forum/tests.py
+++ backend/learning_forum/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
+from unittest import TestCase as UnitTestCase
 from datetime import datetime
 import learning_forum.utils as utils
+from learning_profile.models import  User
+from learning_forum.models import Posts, Comments, Like, Subscription
 import pytz
 
 
-class TestForum(TestCase):
+class TestForumUtils(UnitTestCase):
 
     def setUp(self):
         self.tz = pytz.timezone("Asia/Shanghai")  # unite time zone
@@ -78,4 +81,112 @@ class TestForum(TestCase):
         self.assertEqual(utils.get_progress_msg(1), "To Do")
         self.assertEqual(utils.get_progress_msg(2), "In Progress")
         self.assertEqual(utils.get_progress_msg(3), "Done")
-        
+
+
+user_id = 0
+post_id = 0
+comment_id = 0
+
+
+class TestForumModels(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(
+            username="test1234",
+            password="1234",
+            email="test1234@gmail.com",
+            is_superuser=False,
+            is_active=True,
+        )
+        post = Posts.objects.create(
+            user_id=user.id,
+            content="How you like that",
+            status=1,
+            active=1,
+            likes=0,
+            post_type=1,
+        )
+        Posts.objects.create(
+            user_id=user.id,
+            content="We dont talk anymore",
+            status=2,
+            active=1,
+            likes=10,
+            post_type=1,
+        )
+        global user_id, post_id, comment_id
+        user_id = user.id
+        post_id = post.id
+
+    def test_set_up(self):
+        global user_id, post_id
+        self.assertNotEqual(user_id, 0)
+        self.assertNotEqual(post_id, 0)
+
+    def test_retrieve_posts_desc_length(self):
+        posts = Posts.objects.get_all_posts_desc()
+        self.assertEqual(len(posts), 2)
+
+    def test_retrieve_posts_public_desc_length(self):
+        posts = Posts.objects.get_all_posts_desc_public()
+        self.assertEqual(len(posts), 1)
+
+    def test_get_post_by_uid(self):
+        global user_id
+        posts = Posts.objects.get_posts_by_uid(user_id)
+        self.assertEqual(len(posts), 2)
+
+    def test_get_post_by_uid_false(self):
+        posts = Posts.objects.get_posts_by_uid(0)
+        self.assertEqual(len(posts), 0)
+
+    def test_ban_post(self):
+        global post_id
+        Posts.objects.ban_a_post_by_pid(post_id)
+        post = Posts.objects.get(pk=post_id)
+        self.assertFalse(post.active)
+
+    def test_ban_not_exist(self):
+        self.assertFalse(Posts.objects.ban_a_post_by_pid(0))
+
+    def test_ban_invalid_param(self):
+        self.assertFalse(Posts.objects.ban_a_post_by_pid("0"))
+
+    def test_unban_post(self):
+        global post_id
+        Posts.objects.ban_a_post_by_pid(post_id)
+        Posts.objects.restore_a_post_by_pid(post_id)
+        post = Posts.objects.get(pk=post_id)
+        self.assertTrue(post.active)
+
+    def test_unban_post_not_exist(self):
+        self.assertFalse(Posts.objects.restore_a_post_by_pid(0))
+
+    def test_unban_post_invalid_param(self):
+        self.assertFalse(Posts.objects.restore_a_post_by_pid("0"))
+
+    def test_report_post(self):
+        global post_id
+        Posts.objects.report_post(post_id)
+        post = Posts.objects.get(pk=post_id)
+        self.assertEqual(post.report_times, 1)
+
+    def test_report_clear_after_ban(self):
+        global post_id
+        Posts.objects.report_post(post_id)
+        Posts.objects.ban_a_post_by_pid(post_id)
+        post = Posts.objects.get(pk=post_id)
+        self.assertEqual(post.report_times, 0)
+
+    def test_like_post(self):
+        global post_id
+        Posts.objects.like_post(post_id)
+        post = Posts.objects.get(pk=post_id)
+        self.assertEqual(post.likes, 1)
+
+    def test_dislike_post(self):
+        global post_id
+        Posts.objects.like_post(post_id)
+        Posts.objects.dislike_post(post_id)
+        post = Posts.objects.get(pk=post_id)
+        self.assertEqual(post.likes, 0)
