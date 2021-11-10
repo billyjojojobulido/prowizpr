0252ec42bfe174902ed7309d11c0557d88786ced
Yanhao Xu <xuyanhao2000@gmail.com>
2021-11-11 02:12:24 +0800

    goal test done + profile basic test

--- backend/learning_goal/tests.py
+++ backend/learning_goal/tests.py
@@ -3,8 +3,8 @@ from unittest import TestCase as UnitTestCase
 from datetime import datetime
 import learning_forum.utils as utils
 from learning_profile.models import User
-from learning_forum.models import Posts, Comments, Like, Subscription
-from learning_goal.models import Goals
+from learning_forum.models import Posts
+from learning_goal.models import Goals,Tasks
 import pytz
 
 
@@ -86,3 +86,93 @@ class TestGoalUtils(UnitTestCase):
     def test_publish_msg(self):
         self.assertEqual(utils.get_publish_msg(1), "Public")
         self.assertEqual(utils.get_publish_msg(2), "Private")
+
+    
+user_id = 0
+post_id = 0
+goal_id = 0
+task_id = 0
+
+
+
+class TestGoalModels(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(
+            username="test1234",
+            password="1234",
+            email="test1234@gmail.com",
+            first_name="Unit",
+            last_name="Testing",
+            is_superuser=False,
+            is_active=True,
+        )
+    
+        post = Posts.objects.create(
+            user_id=user.id,
+            content="How you like that",
+            status=1,
+            active=1,
+            likes=10,
+            post_type=1,
+        )
+        
+        goal = Goals.objects.create(
+            likes=10,
+            publish_status=1,
+            description="Something",
+            post_id=post.id,
+        )
+
+        task = Tasks.objects.create(
+            content = "Test",
+            status = 1,
+            goal_id = goal.id,
+        )
+        global user_id, post_id, goal_id,task_id
+        user_id = user.id
+        post_id = post.id
+        goal_id = goal.id
+        task_id = task.id
+
+    def test_set_up(self):
+        global post_id, goal_id
+        self.assertNotEqual(post_id, 0)
+        self.assertNotEqual(goal_id, 0)
+
+    def test_retrieve_goals_desc_length(self):
+        goals = Goals.objects.get_all_goals_desc()
+        self.assertEqual(len(goals), 1)
+
+    def test_check_likes(self):
+        global goal_id
+        Goals.objects.check_goals_liked(goal_id)
+        goal = Goals.objects.get(pk=goal_id)
+        self.assertEqual(goal.likes , 10)
+    def test_check_likes_fail(self):
+        global goal_id
+        self.assertFalse( Goals.objects.check_goals_liked(0))
+
+
+    def test_get_full_name_by_pid(self):
+        global goal_id
+        first_name, last_name = Goals.objects.get_full_name(goal_id)
+        self.assertEqual(first_name, "Unit")
+        self.assertEqual(last_name, "Testing")
+
+    def test_add_goal(self):
+        global post_id
+        Goals.objects.add_goal(post_id,"test")
+        goals = Goals.objects.get_all_goals_desc()
+        self.assertEqual(len(goals) , 2)
+
+    def test_add_goal_fail(self):
+        self.assertFalse(Goals.objects.add_goal(0,'test'))
+
+    
+    def test_get_task_from_pid(self):
+        global post_id
+        tasks = Tasks.objects.get_tasks_from_pid(post_id)
+        self.assertEqual(len(tasks) , 1)
+
+
--- backend/learning_profile/tests.py
+++ backend/learning_profile/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from unittest import TestCase as UnitTestCase
+from datetime import datetime
 
-# Create your tests here.
+from learning_profile.models import User
+from learning_profile.forms import CustomUserCreationForm
+
+import pytz
+
+user_id = 0
+
+class TestProfileModels(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(
+            username="test1234",
+            password="1234",
+            email="test1234@gmail.com",
+            first_name="Unit",
+            last_name="Testing",
+            is_superuser=False,
+            is_active=True,
+        )
+        global user_id
+        user_id = user.id
+    
+    def test_set_up(self):
+        global user_id
+        self.assertNotEqual(user_id, 0)
+
+    def test_user_name(self):
+        global user_id
+        self.assertEqual(User.objects.get(pk=user_id).username,"test1234")
+
+class TestForm(TestCase):
+    def setUp(self):
+        
