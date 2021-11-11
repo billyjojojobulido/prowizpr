329b72eaa07411397515081aaae4b813b710b006
Baocheng Wang <bwan3675@uni.sydney.edu.au>
2021-11-11 19:08:53 +0800

    forum test completed

--- backend/learning_forum/tests.py
+++ backend/learning_forum/tests.py
@@ -1,11 +1,9 @@
-from django.test import TestCase
-from unittest import TestCase as UnitTestCase
 from datetime import datetime
-import learning_forum.utils as utils
-from learning_profile.models import User
-from learning_forum.models import Posts, Comments, Like, Subscription
-from learning_goal.models import Goals
+from unittest import TestCase as UnitTestCase
 import pytz
+from django.test import TestCase, RequestFactory
+from learning_goal.models import Goals
+from .views import *
 
 
 class TestForumUtils(UnitTestCase):
@@ -86,16 +84,13 @@ class TestForumUtils(UnitTestCase):
     def test_publish_msg(self):
         self.assertEqual(utils.get_publish_msg(1), "Public")
         self.assertEqual(utils.get_publish_msg(2), "Private")
-    
-user_id = 0
-post_id = 0
-comment_id = 0
 
 
 class TestForumModels(TestCase):
 
     def setUp(self):
-        user = User.objects.create(
+        self.factory = RequestFactory()
+        self.user = User.objects.create(
             username="test1234",
             password="1234",
             email="test1234@gmail.com",
@@ -104,7 +99,7 @@ class TestForumModels(TestCase):
             is_superuser=False,
             is_active=True,
         )
-        banned_user = User.objects.create(
+        self.banned_user = User.objects.create(
             username="test5678",
             password="1234",
             email="test5678@gmail.com",
@@ -113,51 +108,163 @@ class TestForumModels(TestCase):
             is_superuser=False,
             is_active=False,
         )
-        post = Posts.objects.create(
-            user_id=user.id,
+        self.post = Posts.objects.create(
+            user_id=self.user.id,
             content="How you like that",
             status=1,
             active=1,
             likes=0,
             post_type=1,
         )
-        Posts.objects.create(
-            user_id=user.id,
+        self.private_post = Posts.objects.create(
+            user_id=self.user.id,
             content="We dont talk anymore",
             status=2,
             active=1,
             likes=10,
             post_type=1,
         )
-        comment = Comments.objects.create(
-            user_id=user.id,
-            post_id=post.id,
+        self.comment = Comments.objects.create(
+            user_id=self.user.id,
+            post_id=self.post.id,
             content="Hello World",
             status=1,
             likes=0,
         )
-        Comments.objects.create(
-            user_id=banned_user.id,
-            post_id=post.id,
+        self.another_comment = Comments.objects.create(
+            user_id=self.banned_user.id,
+            post_id=self.post.id,
             content="Goodbye World",
             status=1,
             likes=0,
         )
-        Goals.objects.create(
+        self.goal = Goals.objects.create(
             likes=0,
             publish_status=1,
             description="Something",
-            post_id=post.id,
+            post_id=self.post.id,
         )
-        global user_id, post_id, comment_id
-        user_id = user.id
-        post_id = post.id
-        comment_id = comment.id
+
+    def test_show(self):
+        payload = {
+            "user_id": self.user.id,
+        }
+        request = self.factory.post(
+            '/forum/show',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        response = show(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_goal(self):
+        payload = {
+            "pid": self.post.id,
+        }
+        request = self.factory.post(
+            '/forum/retrieve_goal',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        response = retrieve_goal(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_comment(self):
+        payload = {
+            "pid": self.post.id,
+            "uid": self.user.id,
+        }
+        request = self.factory.post(
+            '/forum/retrieve_comment',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        response = retrieve_comment(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_report_post(self):
+        payload = {
+            "pid": self.post.id,
+        }
+        request = self.factory.post(
+            '/forum/report_post',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        response = report_post(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_write_comment(self):
+        payload = {
+            "user_id": self.user.id,
+            "pid": self.post.id,
+            "content": "Hello World",
+        }
+        request = self.factory.post(
+            '/forum/write_comment',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        response = write_comment(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_like_post(self):
+        payload = {
+            "user_id": self.user.id,
+            "pid": self.post.id,
+            "like": 1,
+        }
+        request = self.factory.post(
+            '/forum/like_post',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        response = like_post(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_dislike_post(self):
+        payload = {
+            "user_id": self.user.id,
+            "pid": self.post.id,
+            "like": 0,
+        }
+        request = self.factory.post(
+            '/forum/like_post',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        response = like_post(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_subscribe(self):
+        payload = {
+            "user_id": self.user.id,
+            "pid": self.post.id,
+        }
+        request = self.factory.post(
+            '/forum/subscribe_post',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        response = subscribe_post(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_unsubscribe(self):
+        payload = {
+            "user_id": self.user.id,
+            "pid": self.post.id,
+        }
+        request = self.factory.post(
+            '/forum/unsubscribe_post',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        response = unsubscribe_post(request)
+        self.assertEqual(response.status_code, 200)
 
     def test_set_up(self):
-        global user_id, post_id
-        self.assertNotEqual(user_id, 0)
-        self.assertNotEqual(post_id, 0)
+        self.assertNotEqual(self.user.id, 0)
+        self.assertNotEqual(self.post.id, 0)
 
     def test_retrieve_posts_desc_length(self):
         posts = Posts.objects.get_all_posts_desc()
@@ -168,8 +275,7 @@ class TestForumModels(TestCase):
         self.assertEqual(len(posts), 1)
 
     def test_get_post_by_uid(self):
-        global user_id
-        posts = Posts.objects.get_posts_by_uid(user_id)
+        posts = Posts.objects.get_posts_by_uid(self.user.id)
         self.assertEqual(len(posts), 2)
 
     def test_get_post_by_uid_false(self):
@@ -177,9 +283,8 @@ class TestForumModels(TestCase):
         self.assertEqual(len(posts), 0)
 
     def test_ban_post(self):
-        global post_id
-        Posts.objects.ban_a_post_by_pid(post_id)
-        post = Posts.objects.get(pk=post_id)
+        Posts.objects.ban_a_post_by_pid(self.post.id)
+        post = Posts.objects.get(pk=self.post.id)
         self.assertFalse(post.active)
 
     def test_ban_not_exist(self):
@@ -189,10 +294,9 @@ class TestForumModels(TestCase):
         self.assertFalse(Posts.objects.ban_a_post_by_pid("0"))
 
     def test_unban_post(self):
-        global post_id
-        Posts.objects.ban_a_post_by_pid(post_id)
-        Posts.objects.restore_a_post_by_pid(post_id)
-        post = Posts.objects.get(pk=post_id)
+        Posts.objects.ban_a_post_by_pid(self.post.id)
+        Posts.objects.restore_a_post_by_pid(self.post.id)
+        post = Posts.objects.get(pk=self.post.id)
         self.assertTrue(post.active)
 
     def test_unban_post_not_exist(self):
@@ -201,47 +305,40 @@ class TestForumModels(TestCase):
     def test_unban_post_invalid_param(self):
         self.assertFalse(Posts.objects.restore_a_post_by_pid("0"))
 
-    def test_report_post(self):
-        global post_id
-        Posts.objects.report_post(post_id)
-        post = Posts.objects.get(pk=post_id)
+    def test_report_post_orm(self):
+        Posts.objects.report_post(self.post.id)
+        post = Posts.objects.get(pk=self.post.id)
         self.assertEqual(post.report_times, 1)
 
     def test_report_clear_after_ban(self):
-        global post_id
-        Posts.objects.report_post(post_id)
-        Posts.objects.ban_a_post_by_pid(post_id)
-        post = Posts.objects.get(pk=post_id)
+        Posts.objects.report_post(self.post.id)
+        Posts.objects.ban_a_post_by_pid(self.post.id)
+        post = Posts.objects.get(pk=self.post.id)
         self.assertEqual(post.report_times, 0)
 
-    def test_like_post(self):
-        global post_id
-        Posts.objects.like_post(post_id)
-        post = Posts.objects.get(pk=post_id)
+    def test_like_post_orm(self):
+        Posts.objects.like_post(self.post.id)
+        post = Posts.objects.get(pk=self.post.id)
         self.assertEqual(post.likes, 1)
 
-    def test_dislike_post(self):
-        global post_id
-        Posts.objects.like_post(post_id)
-        Posts.objects.dislike_post(post_id)
-        post = Posts.objects.get(pk=post_id)
+    def test_dislike_post_orm(self):
+        Posts.objects.like_post(self.post.id)
+        Posts.objects.dislike_post(self.post.id)
+        post = Posts.objects.get(pk=self.post.id)
         self.assertEqual(post.likes, 0)
 
     def test_dislike_post_below_zero(self):
-        global post_id
-        Posts.objects.dislike_post(post_id)
-        post = Posts.objects.get(pk=post_id)
+        Posts.objects.dislike_post(self.post.id)
+        post = Posts.objects.get(pk=self.post.id)
         self.assertEqual(post.likes, 0)
 
     def test_get_full_name_by_pid(self):
-        global post_id
-        first_name, last_name = Posts.objects.get_full_name_by_pid(post_id)
+        first_name, last_name = Posts.objects.get_full_name_by_pid(self.post.id)
         self.assertEqual(first_name, "Unit")
         self.assertEqual(last_name, "Testing")
 
     def test_get_all_comments_by_pid(self):
-        global post_id
-        comments = Comments.objects.get_all_comments_by_pid(post_id)
+        comments = Comments.objects.get_all_comments_by_pid(self.post.id)
         self.assertEqual(len(comments), 2)
 
     def test_get_all_comments_by_invalid_pid(self):
@@ -249,43 +346,36 @@ class TestForumModels(TestCase):
         self.assertEqual(len(comments), 0)
 
     def test_get_comments_by_pid(self):
-        global post_id
-        comments = Comments.objects.get_comments_by_pid_transmit(post_id)
+        comments = Comments.objects.get_comments_by_pid_transmit(self.post.id)
         self.assertEqual(len(comments), 1)
 
     def test_get_comments_by_pid_invalid(self):
         comments = Comments.objects.get_comments_by_pid_transmit(0)
         self.assertEqual(len(comments), 0)
 
-    def test_write_comment(self):
-        global post_id, user_id
-        self.assertTrue(Comments.objects.write_comment(post_id, user_id, "Test Comment"))
+    def test_write_comment_orm(self):
+        self.assertTrue(Comments.objects.write_comment(self.post.id, self.user.id, "Test Comment"))
 
-    def test_like_post_orm(self):
-        global post_id, user_id
-        Like.objects.like_post(post_id, user_id)
-        likes = Like.objects.filter(post_id=post_id, user_id=user_id)
+    def test_like_post_orm_2(self):
+        Like.objects.like_post(self.post.id, self.user.id)
+        likes = Like.objects.filter(post_id=self.post.id, user_id=self.user.id)
         self.assertEqual(likes.count(), 1)
 
-    def test_dislike_post_orm(self):
-        global post_id, user_id
-        Like.objects.like_post(post_id, user_id)
-        Like.objects.dislike_post(post_id, user_id)
-        likes = Like.objects.filter(post_id=post_id, user_id=user_id)
+    def test_dislike_post_orm_2(self):
+        Like.objects.like_post(self.post.id, self.user.id)
+        Like.objects.dislike_post(self.post.id, self.user.id)
+        likes = Like.objects.filter(post_id=self.post.id, user_id=self.user.id)
         self.assertEqual(likes.count(), 0)
 
     def test_checked_post_liked_positive(self):
-        global post_id, user_id
-        Like.objects.like_post(post_id, user_id)
-        self.assertEqual(Like.objects.check_post_liked(post_id, user_id), 1)
+        Like.objects.like_post(self.post.id, self.user.id)
+        self.assertEqual(Like.objects.check_post_liked(self.post.id, self.user.id), 1)
 
     def test_checked_post_liked_negative(self):
-        global post_id, user_id
-        self.assertEqual(Like.objects.check_post_liked(post_id, user_id), 0)
+        self.assertEqual(Like.objects.check_post_liked(self.post.id, self.user.id), 0)
 
     def test_is_subscribed_negative(self):
-        global post_id, user_id
-        self.assertFalse(Subscription.objects.is_subscribed(post_id, user_id))
+        self.assertFalse(Subscription.objects.is_subscribed(self.post.id, self.user.id))
 
     def test_is_subscribed_invalid(self):
         self.assertFalse(Subscription.objects.is_subscribed(None, None))
